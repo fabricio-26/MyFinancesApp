ecbd76dece767ff8d076a39ce1bb83c86fecf945
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-16 22:51:18 -0300

    Rotas autenticadas

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,13 +2,38 @@ import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Home from '../pages/Home'
+import Profile from "../pages/Profile";
+import New from "../pages/New";
+
 
 const AppDrawer = createDrawerNavigator();
 
-function AppRoutes(){
-    return(
-        <AppDrawer.Navigator>
-            <AppDrawer.Screen name="Home" component={Home}/>
+function AppRoutes() {
+    return (
+        <AppDrawer.Navigator
+        screenOptions={{
+            drawerStyle: {
+              backgroundColor: '#171717',
+            },
+            drawerLabelStyle:{
+                fontWeight: 'bold'
+            },
+            drawerActiveTintColor:'#fff',
+            drawerActiveBackgroundColor: '#00b94a',
+
+            drawerInactiveBackgroundColor: '#000',
+            drawerInactiveTintColor: '#ddd',
+            drawerItemStyle:{
+                marginVertical: 5
+            }
+          }}
+          
+
+        >
+    
+            <AppDrawer.Screen name="Home" component={Home} />
+            <AppDrawer.Screen name="Registrar" component={New} />
+            <AppDrawer.Screen name="Perfil" component={Profile} />
         </AppDrawer.Navigator>
     )
 }
