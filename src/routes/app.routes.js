51eb0589ceddbda5a930e6729b670c705feece78
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-20 17:24:50 -0300

    App finalizado

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer'
 
+import CustomDrawer from "../components/CustomDrawer";
+
 import Home from '../pages/Home'
 import Profile from "../pages/Profile";
 import New from "../pages/New";
@@ -9,71 +11,74 @@ import New from "../pages/New";
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
-    return (
-        <AppDrawer.Navigator
-        screenOptions={{
-            drawerStyle: {
-              backgroundColor: '#171717',
-            },
-            drawerLabelStyle:{
-                fontWeight: 'bold'
-            },
-            drawerActiveTintColor:'#fff',
-            drawerActiveBackgroundColor: '#00b94a',
+  return (
+    <AppDrawer.Navigator
 
-            drawerInactiveBackgroundColor: '#000',
-            drawerInactiveTintColor: '#ddd',
-            drawerItemStyle:{
-                marginVertical: 5
-            }
-          }}
-          
+    drawerContent={ (props) => <CustomDrawer {...props}/>}
 
-        >
-    
-            <AppDrawer.Screen name="Home" component={Home} 
-            options={{
-                headerStyle:{
-                  backgroundColor: '#131313',
-                  borderBottomColor: '#00b94a', ///nao esta pegando
-                  borderBottomWidth: 0,
-                  shadowColor: 'transparent' //apagar a borda natural == 0
-                },
-                headerTintColor: '#FFF',
-                headerBackTitleVisible: false,
-                headerTitle: ''
-              }}
-            />
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#171717',
+        },
+        drawerLabelStyle: {
+          fontWeight: 'bold'
+        },
+        drawerActiveTintColor: '#fff',
+        drawerActiveBackgroundColor: '#00b94a',
 
-            <AppDrawer.Screen name="Registrar" component={New} 
-            options={{
-                headerStyle:{
-                  backgroundColor: '#131313',
-                  borderBottomColor: '#00b94a', ///nao esta pegando
-                  borderBottomWidth: 0,
-                  shadowColor: 'transparent'
-                },
-                headerTintColor: '#FFF',
-                headerBackTitleVisible: false,
-                headerTitle: ''
-              }}
-            />
+        drawerInactiveBackgroundColor: '#000',
+        drawerInactiveTintColor: '#ddd',
+        drawerItemStyle: {
+          marginVertical: 5
+        }
+      }}
 
-            <AppDrawer.Screen name="Perfil" component={Profile} 
-            options={{
-                headerStyle:{
-                  backgroundColor: '#131313',
-                  borderBottomColor: '#00b94a', ///nao esta pegando
-                  borderBottomWidth: 0,
-                  shadowColor: 'transparent'
-                },
-                headerTintColor: '#FFF',
-                headerBackTitleVisible: false,
-                headerTitle: ''
-              }}
-            />
-        </AppDrawer.Navigator>
-    )
+
+    >
+
+      <AppDrawer.Screen name="Home" component={Home}
+        options={{
+          headerStyle: {
+            backgroundColor: '#131313',
+            borderBottomColor: '#00b94a', ///nao esta pegando
+            borderBottomWidth: 0,
+            shadowColor: 'transparent' //apagar a borda natural == 0
+          },
+          headerTintColor: '#FFF',
+          headerBackTitleVisible: false,
+          headerTitle: ''
+        }}
+      />
+
+      <AppDrawer.Screen name="Registrar" component={New}
+        options={{
+          headerStyle: {
+            backgroundColor: '#131313',
+            borderBottomColor: '#00b94a', ///nao esta pegando
+            borderBottomWidth: 0,
+            shadowColor: 'transparent'
+          },
+          headerTintColor: '#FFF',
+          headerBackTitleVisible: false,
+          headerTitle: ''
+        }}
+      />
+
+      <AppDrawer.Screen name="Perfil" component={Profile}
+        options={{
+          headerStyle: {
+            backgroundColor: '#131313',
+            borderBottomColor: '#00b94a', ///nao esta pegando
+            borderBottomWidth: 0,
+            shadowColor: 'transparent'
+          },
+          headerTintColor: '#FFF',
+          headerBackTitleVisible: false,
+          headerTitle: ''
+        }}
+      />
+    </AppDrawer.Navigator>
+  )
 }
 
 export default AppRoutes
