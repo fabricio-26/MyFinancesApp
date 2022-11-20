3801d201155fe1965d42503b9ac996e22dc16427
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-20 11:29:28 -0300

    pickerDate/calendario

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,50 +1,56 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Alert } from 'react-native';
+import { Alert, Platform, TouchableOpacity } from 'react-native';
 import firebase from '../../services/firebaseConnection';
 import { format, isBefore } from 'date-fns';
 
 import HistoricoListBalance from '../../components/HistoricoListBalance';
+import DatePicker from '../../components/DatePicker';
 import { AuthContext } from '../../contexts/auth'
 
-import { Background, Container, Nome, Saldo, Title, List } from './styles'
+import { Background, Container, Nome, Saldo, Title, List, Area } from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { onChange } from 'deprecated-react-native-prop-types/DeprecatedTextInputPropTypes';
 
 export default function Home() {
   const [historico, setHistorico] = useState([])
   const [saldo, setSaldo] = useState(0)
+  const [newDate, setNewDate] = useState(new Date())
+  const [showCalendar, setShowCalendar] = useState(false)
+
 
   const { user } = useContext(AuthContext)
   const uid = user && user.uid;
 
-  useEffect(()=>{
-    async function loadList(){
-      await firebase.database().ref('users').child(uid).on('value', (snapshot)=>{
+  useEffect(() => {
+    async function loadList() {
+      await firebase.database().ref('users').child(uid).on('value', (snapshot) => {
         setSaldo(snapshot.val().saldo)
       })
 
       await firebase.database().ref('historico')
-      .child(uid)
-      .orderByChild('date').equalTo(format(new Date, 'dd/MM/yyyy'))
-      .limitToLast(15).on('value', (snapshot)=>{
-        setHistorico([]);
+        .child(uid)
+        .orderByChild('date').equalTo(format(newDate, 'dd/MM/yyyy'))
+        .limitToLast(15).on('value', (snapshot) => {
+          setHistorico([]);
 
-        snapshot.forEach((childItem)=> {
-          let list = {
-            key: childItem.key,
-            tipo: childItem.val().tipo,
-            valor: childItem.val().valor,
-            date: childItem.val().date
-          };
+          snapshot.forEach((childItem) => {
+            let list = {
+              key: childItem.key,
+              tipo: childItem.val().tipo,
+              valor: childItem.val().valor,
+              date: childItem.val().date
+            };
 
-          setHistorico(oldArray => [...oldArray, list].reverse())
+            setHistorico(oldArray => [...oldArray, list].reverse())
+          })
         })
-      })
     }
 
     loadList()
-  },[])
+  }, [newDate])
 
 
-  function handleDelete(data){
+  function handleDelete(data) {
 
     // Pegando data do item
     const [diaItem, mesItem, anoItem] = data.date.split('/');
@@ -59,7 +65,7 @@ export default function Home() {
 
 
 
-    if(isBefore(dateItem, dateHoje)){
+    if (isBefore(dateItem, dateHoje)) {
       // Se a data do registro ja passou, vai entrar aqui
       alert("Voce não pode excluir um registro antigo!")
       return;
@@ -83,19 +89,32 @@ export default function Home() {
   }
 
 
-  async function handleDeleteSuccess(data){
+  async function handleDeleteSuccess(data) {
     await firebase.database().ref('historico')
-    .child(uid).child(data.key).remove()
-    .then( async ()=> {
-      let saldoAtual = saldo;
-      data.tipo === 'despesa' ? saldoAtual += parseFloat(data.valor) : saldoAtual -= parseFloat(data.valor)
+      .child(uid).child(data.key).remove()
+      .then(async () => {
+        let saldoAtual = saldo;
+        data.tipo === 'despesa' ? saldoAtual += parseFloat(data.valor) : saldoAtual -= parseFloat(data.valor)
 
-      await firebase.database().ref('users').child(uid)
-      .child('saldo').set(saldoAtual);
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
+        await firebase.database().ref('users').child(uid)
+          .child('saldo').set(saldoAtual);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  function handleShowPicker(){
+    setShowCalendar(true)
+  }
+
+  function handleClose(){
+    setShowCalendar(false)
+  }
+
+  const onChange = (date) => {
+    setShowCalendar(Platform.OS === 'ios')
+    setNewDate(date)
   }
 
   return (
@@ -105,14 +124,27 @@ export default function Home() {
         <Saldo>R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</Saldo>
       </Container>
 
-      <Title>Ultimas movimentaçoes</Title>
+      <Area>
+        <TouchableOpacity onPress={handleShowPicker}>
+          <Icon name='event' size={30} color="#FFF" />
+        </TouchableOpacity>
+        <Title>Ultimas movimentaçoes</Title>
+      </Area>
 
       <List
         showsVerticalScrollIndicator={false}
         data={historico}
         keyExtractor={item => item.key}
-        renderItem={({ item }) => (<HistoricoListBalance data={item} deleteItem={handleDelete}/>)}
+        renderItem={({ item }) => (<HistoricoListBalance data={item} deleteItem={handleDelete} />)}
       />
+
+      {showCalendar && (
+        <DatePicker
+        onClose={handleClose}
+        date={newDate}
+        onChange={onChange}
+        />
+      )}
 
     </Background>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -24,7 +24,7 @@ font-weight: bold;
 `;
 
 export const Title = styled.Text`
-margin-left: 15px;
+margin-left: 5px;
 color: #00b94a;
 margin-bottom: 10px;
 `;
@@ -38,4 +38,11 @@ border-top-left-radius: 15px;
 border-top-right-radius: 15px;
 margin-left: 8px;
 margin-right: 8px;
+`;
+
+export const Area = styled.View`
+flex-direction: row;
+margin-left: 15px;
+align-items: center;
+margin-bottom: 5px;
 `;
