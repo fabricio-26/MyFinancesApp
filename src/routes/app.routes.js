19c406c4a666e33eadd58ab7a2376dc97d5e4d95
fabricio-26 <fabricioohh@gmail.com>
2022-11-21 13:30:02 -0300

    adicionado navegaçao em tabs

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import CustomDrawer from "../components/CustomDrawer";
 
@@ -7,8 +9,41 @@ import Home from '../pages/Home'
 import Profile from "../pages/Profile";
 import New from "../pages/New";
 
-
+const Tab = createBottomTabNavigator()
 const AppDrawer = createDrawerNavigator();
+
+const icons = {
+  Home: {
+    name: 'ios-home'
+  },
+  Profile: {
+    name: 'ios-people'
+  },
+  New: {
+    name: 'ios-call'
+  }
+}
+
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const { name } = icons[route.name];
+          return <Icon name={name} color={color} size={size} />
+        }
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false}}/>
+      <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false}}/>
+      <Tab.Screen name="New" component={New} options={{ headerShown: false}}/>
+    </Tab.Navigator>
+
+  )
+}
+
+
 
 function AppRoutes() {
   return (
@@ -36,7 +71,7 @@ function AppRoutes() {
 
     >
 
-      <AppDrawer.Screen name="Home" component={Home}
+      <AppDrawer.Screen name="Home" component={Tabs}
         options={{
           headerStyle: {
             backgroundColor: '#131313',
