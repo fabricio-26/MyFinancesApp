b23f997ffa23b6573ec0dfd8d691b60c39b1ab49
fabricio-26 <fabricioohh@gmail.com>
2022-11-23 17:58:29 -0300

    update Readme

--- src/components/SlideCarteiras/index.js
+++ src/components/SlideCarteiras/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableWithoutFeedback, ScrollView } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { Box1, Box2, Box3, Box4, } from './styles'
 
@@ -8,10 +8,17 @@ export default function SlideCarteiras(props) {
     return (
 
         <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
         >
-            <Box1></Box1>
+            <Box1>
+
+                <Icon
+                    name='Power'
+                    color="#fff"
+                    size={20}
+                />
+            </Box1>
             <Box2></Box2>
             <Box3></Box3>
             <Box4></Box4>
